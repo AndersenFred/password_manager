1c64b98483797b58c37ac6c317f1be4be3420dca
Frederik Andersen <82170487+AndersenFred@users.noreply.github.com>
2022-07-13 23:56:15 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import password_manager as pm
+import manager_GUI as mg
+import sys
+from PyQt6.QtWidgets import QApplication
+
+
+if __name__ == '__main__':
+    w = QApplication(sys.argv)
+    app = mg.manager_gui()
+    app.show()
+    w.exec()
+    mg.manager_gui()
